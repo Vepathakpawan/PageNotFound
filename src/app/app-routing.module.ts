5e3424b98d29e7454f073cfc4a545f85e1ca2661
Vepathakpawan <pawanved007@gmail.com>
2021-04-25 03:21:48 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent} from './home/home.component';
+import { ShopeComponent} from './shope/shope.component';
+import { HeaderComponent} from './header/header.component';
+import { FooterComponent} from './footer/footer.component';
+import { PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import { combineLatest } from 'rxjs';
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path: 'shop',
+    component: ShopeComponent
+  },
+  {
+    path: 'header',
+    component: HeaderComponent
+  },
+  {
+    path: 'footer',
+    component: FooterComponent
+  },
+{
+  path: '**',
+  component: PageNotFoundComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
